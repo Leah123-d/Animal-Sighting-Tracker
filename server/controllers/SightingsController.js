97662be6b5a49c9955e911fd262cd90fb5ea5953
Leah123-d <95731243+Leah123-d@users.noreply.github.com>
2025-03-20 21:03:17 -0700

    completed route setup

--- server/controllers/SightingsController.js
+++ server/controllers/SightingsController.js
@@ -11,8 +11,8 @@ export const getSightings = async (req,res) => {
 
 export const getOneSighting  = async (req,res) => {
   try{
-    const { date_sighted } = req.params; 
-    const result = await dbConnection.query(`SELECT * FROM sightings WHERE date_sighted = $1`, [date_sighted]);
+    const { individual_seen } = req.params; 
+    const result = await dbConnection.query(`SELECT * FROM sightings WHERE individual_seen = $1`, [individual_seen]);
 
     if(result.rows.length === 0){
         return res.send('sighting not found');
@@ -20,6 +20,7 @@ export const getOneSighting  = async (req,res) => {
 
     res.json(result.rows[0]);
     }catch (error){
+
         console.error('No sighting found', error);
     }
 }
@@ -33,7 +34,7 @@ export const createSighting  = async (req,res) => {
     );
     console.log(result);
     
-    res.json({ message:`new sighting ${result.rows[0].title} was added with date_sighted ${result.rows[0].date_sighted}`})
+    res.json({ message:`new sighting ${result.rows[0].individual_seen} was added with individual_seen ${result.rows[0].date_sighted}`})
     }catch (error) {
         console.error('Error creating new sighting: ', error);
     }
@@ -41,10 +42,10 @@ export const createSighting  = async (req,res) => {
 
 export const updateSighting  = async (req,res) => {
   try{
-    const { date_sighted } = req.params; //how to find the row we want to update
-    const { individual_seen } = req.body; //the column we are updating
+    const { individual_seen } = req.params; //how to find the row we want to update
+    const { location_of_sighting } = req.body; //the column we are updating
 
-    const result = await dbConnection.query("UPDATE sightings SET individual_seen = $1 WHERE date_sighted = $2 RETURNING *", [date_sighted, individual_seen]);
+    const result = await dbConnection.query("UPDATE sightings SET location_of_sighting = $1 WHERE individual_seen = $2 RETURNING *", [location_of_sighting, individual_seen]);
     res.json(result.rows[0]);
   }catch (error) {
     console.error('Error updating sighting: ', error);
@@ -52,14 +53,14 @@ export const updateSighting  = async (req,res) => {
 }
 
 export const deleteSighting  = async (req,res) => {
+  const { individual_seen } = req.params;
   try{
-    const { date_sighted } = req.params;
-    const result = await dbConnection.query("DELETE FROM indiviudals WHERE date_sighted = $1 RETURNING *", [date_sighted]);
+    const result = await dbConnection.query("DELETE FROM sightings WHERE individual_seen = $1 RETURNING *", [individual_seen]);
     if(result.rowCount === 0){
         return res.send(`species not found`);
     }
-    res.send(`species with common_name ${date_sighted} has been deleted`);
+    res.send(`species with common_name ${individual_seen} has been deleted`);
     } catch (error){
-        console.error(`Could not locate species with common_name: ${date_sighted}: `, error);
+        console.error(`Could not locate species with common_name: ${individual_seen}: `, error);
     }
 }
--- server/routes/SightingsRoute.js
+++ server/routes/SightingsRoute.js
@@ -10,10 +10,11 @@ const router = express.Router();
 
 router.get("/", getSightings);
 
-router.get("/:date_sighted", getOneSighting);
+router.get("/:individual_seen", getOneSighting);
 router.post("/", createSighting);
-router.put("/:date_sighted", updateSighting);
-router.delete("/:date_sighted", deleteSighting);
+router.put("/:individual_seen", updateSighting);
+router.delete("/:individual_seen", deleteSighting);
 // add 1 function that uses a join it can be a get
+//can I make my routes more dynamic where it can accept more than 1 query parameter?
 
 export default router;
